fd13b6947f0da36d32a32eb25074dfda3174c0a9
Boseok Lee <boseok.lee@hotmail.com>
2026-03-27 10:21:42 +0900

    원복

--- src/face_detect.py
+++ src/face_detect.py
@@ -1,37 +1,30 @@
+# pip install retina-face
+# we recommand tensorflow==2.15
+from retinaface import RetinaFace
 import sys
-import mediapipe as mp
 from PIL import Image
 import numpy as np
 
-
 def get_mask_coord(image_path):
-    img = Image.open(image_path).convert("RGB")
-    img_np = np.array(img)
-    height, width, _ = img_np.shape
 
-    detector = mp.solutions.face_detection.FaceDetection(
-        model_selection=1, min_detection_confidence=0.5
-    )
-    results = detector.process(img_np)
-    detector.close()
+  img = Image.open(image_path).convert("RGB")
+  img = np.array(img)[:,:,::-1]
+  if img is None:
+    raise ValueError(f"Exception while loading {img}")
 
-    if not results.detections:
-        print(f'{image_path} has no face detected!')
-        return None
+  height, width, _ = img.shape
 
-    det = results.detections[0]
-    box = det.location_data.relative_bounding_box
-    x = int(box.xmin * width)
-    y = int(box.ymin * height)
-    x2 = int((box.xmin + box.width) * width)
-    y2 = int((box.ymin + box.height) * height)
+  facial_areas = resp = RetinaFace.detect_faces(img)
+  if len(facial_areas) == 0:
+    print (f'{image_path} has no face detected!')
+    return None
+  else:
+    face = facial_areas['face_1']
+    x,y,x2,y2 = face["facial_area"]
 
-    return y, y2, x, x2, height, width
-
+    return y,y2,x,x2,height,width
 
 if __name__ == "__main__":
-    image_path = sys.argv[1]
-    result = get_mask_coord(image_path)
-    if result:
-        y, y2, x, x2, height, width = result
-        print(y, y2, x, x2, height, width)
+  image_path = sys.argv[1]
+  y,y2,x,x2,height,width = get_mask_coord(image_path)
+  print (y,y2,x,x2,height,width)
